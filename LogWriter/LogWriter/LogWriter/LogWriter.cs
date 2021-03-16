e62df7818a84d4d68d7d0e945281b7b0ea0228b6
ANJU-SAP\Anju-SAP <buvindu.fernando@gmail.com>
2021-03-16 16:08:57 +0530

    no message

---
+++ LogWriter/LogWriter/LogWriter/LogWriter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogWriter
+{
+    public static class LogWriter
+    {       
+        public static void WriteLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                System.IO.Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory+@"\Log");
+                System.IO.Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + @"\Log\"+DateTime.Now.ToString("yyyy-MM-dd"));
+                //sw = new StreamWriter(@"D:\Logfile.txt", true);
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + @"\Log\" + DateTime.Now.ToString("yyyy-MM-dd") + @"\" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Name.ToString() + "_Log.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + " : " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+            }
+        }
+        public static void WriteErrors(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                System.IO.Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + @"\ErrorLog");
+                System.IO.Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + @"\ErrorLog\" + DateTime.Now.ToString("yyyy-MM-dd"));
+                //sw = new StreamWriter(@"D:\Logfile.txt", true);
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + @"\ErrorLog\" + DateTime.Now.ToString("yyyy-MM-dd") + @"\" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Name.ToString() + "_ErrorLog.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + " : " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+            }
+        }
+        public static void WriteFileDocumentWise(string DocNum,string FileName,string Name,string value)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                System.IO.Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + @"\" + FileName);
+                System.IO.Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + @"\"+FileName+@"\" + DateTime.Now.ToString("yyyy-MM-dd"));
+                //sw = new StreamWriter(@"D:\Logfile.txt", true);
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + @"\" + FileName + @"\" + DateTime.Now.ToString("yyyy-MM-dd") + @"\" + FileName + "_" + DocNum + ".txt", true);
+                sw.WriteLine(Name + " : " + value);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
